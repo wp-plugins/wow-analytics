7c59cfa9572e714853370188f6016b01208738e3
Wow Analytics <Wow Analytics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-10 13:13:01 +0000

    updated for new tracking code
    git-svn-id: https://plugins.svn.wordpress.org/wow-analytics/tags/1.1.0@610358 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/trackingcode.php
@@ -0,0 +1,42 @@
+<?php
+
+function wow_wowanalytics_should_output_trackingcode(){
+ 
+    $options = get_option('wow_wowanalytics_options');
+    $clientid_text = trim($options['clientid_text']);
+    $has_clientid = !empty($clientid_text);
+    
+    if (!current_user_can('manage_options') && $has_clientid){
+        wow_wowanalytics_output_trackingcode();
+    }
+}
+
+function wow_wowanalytics_output_trackingcode(){
+    // get the options
+    $options = get_option('wow_wowanalytics_options');
+    $clientid_text = trim($options['clientid_text']);
+    $trackuser_bool = $options['trackuser_bool'];
+	$trackdownloads_bool = $options['track_downloads_bool'];
+?>
+<!-- WOW Tracking Code Start -->
+<script type='text/javascript' src='//t.wowanalytics.co.uk/Scripts/tracker.js'></script>
+<script type='text/javascript'>
+try {
+	// Set the tracking url
+	var tracker = new wowTracking.getTracker('<?php echo $clientid_text; ?>');
+	tracker.trackPageView();
+	<?php
+	if(!$trackuser_bool){?>
+	tracker.disableUserTracking();
+	<?php 
+	}
+	if($trackdownloads_bool){?>
+	tracker.enableDownloadTracking();
+	<?php }	?>
+	
+} catch (err) { }        
+</script>
+<!-- WOW Tracking Code End -->
+<?php
+}
+?>
